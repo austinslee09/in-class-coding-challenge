1b46f0d908366ae64e25e635ac96f7f0fbf1140b
Austin Lee <austinlee@usf.edu>
2025-09-10 18:12:48 -0400

    syntax 3

--- cc_3b.js
+++ cc_3b.js
@@ -1,25 +1,25 @@
-const inventory={
+const inventory=[{
  name:"cow",
   sku:"sku-234",
   price:"1.99",
  stock:"12"
-  }
-{name:"chicken"
-sku:"sku-324"
-price:"2.98"
+  },
+{name:"chicken",
+sku:"sku-324",
+price:"2.98",
 stock:"9"
-}
+},
 
-{name:"dog"
- sku:"sku-445"
- price:"9.99"
+{name:"dog",
+ sku:"sku-445",
+ price:"9.99",
  stock:"4"   
-}
-{name:"cat"
- sku:"sku-905"
- price:"8.92"
+},
+{name:"cat",
+ sku:"sku-905",
+ price:"8.92",
  stock:"89"
-};
+}];
 
 inventory.forEach(obj => console.log (`Name: ${Object.name}` | `Sku: ${Object.sku}` | `price: ${Object.price}` | `stock: ${Object.stock}`));
 
